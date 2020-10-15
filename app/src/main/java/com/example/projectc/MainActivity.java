2708d56cbbeca42b05b05e77d906cb8b64553c3e
wogns2225 <wogns2225@gmail.com>
2020-10-15 21:12:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/projectc/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.projectc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * client 정보를 넣어준다.
+     */
+    private static String OAUTH_CLIENT_ID = "cknsa1k3G_xZP5DOWTBQ";
+    private static String OAUTH_CLIENT_SECRET = "cknsa1k3G_xZP5DOWTBQ";
+    private static String OAUTH_CLIENT_NAME = "무테스트";
+
+    LinearLayout ll_naver_login;
+    Button btn_logout;
+
+    OAuthLogin mOAuthLoginModule;
+    private static Context mContext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContext = getApplicationContext();
+
+        ll_naver_login = findViewById(R.id.ll_naver_login);
+        btn_logout = findViewById(R.id.btn_logout);
+
+        ll_naver_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOAuthLoginModule = OAuthLogin.getInstance();
+                mOAuthLoginModule.init(
+                        mContext
+                        ,OAUTH_CLIENT_ID
+                        ,OAUTH_CLIENT_SECRET
+                        ,OAUTH_CLIENT_NAME
+                        //,OAUTH_CALLBACK_INTENT
+                        // SDK 4.1.4 버전부터는 OAUTH_CALLBACK_INTENT변수를 사용하지 않습니다.
+                );
+
+                @SuppressLint("HandlerLeak")
+                OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+                    @Override
+                    public void run(boolean success) {
+                        Log.i("LoginData", "success :" + success);
+
+                        if (success) {
+                            String accessToken = mOAuthLoginModule.getAccessToken(mContext);
+                            String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
+                            long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
+                            String tokenType = mOAuthLoginModule.getTokenType(mContext);
+
+                            Log.i("LoginData","accessToken : "+ accessToken);
+                            Log.i("LoginData","refreshToken : "+ refreshToken);
+                            Log.i("LoginData","expiresAt : "+ expiresAt);
+                            Log.i("LoginData","tokenType : "+ tokenType);
+
+                        } else {
+                            String errorCode = mOAuthLoginModule
+                                    .getLastErrorCode(mContext).getCode();
+                            String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
+                            Toast.makeText(mContext, "errorCode:" + errorCode
+                                    + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+                        }
+                    };
+                };
+                Log.i("LoginData", "Here");
+                mOAuthLoginModule.startOauthLoginActivity(MainActivity.this, mOAuthLoginHandler);
+            }
+        });
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOAuthLoginModule.logout(mContext);
+                Toast.makeText(MainActivity.this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
